2de7234fcfcd1207f002acc75fe60be1e0061580
Rk Shaju 612 <91930059+professorop@users.noreply.github.com>
2022-05-16 17:40:06 +0600

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,17 +1,17 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start-s'
+        self.StartCommand = 'start'
         self.ListCommand = 's'
-        self.CloneCommand = 'clone-s'
-        self.CountCommand = 'count-s'
-        self.PermissionCommand = 'perm-s'
-        self.DeleteCommand = 'del-s'
-        self.AuthorizeCommand = 'authorize-s'
-        self.UnauthorizeCommand = 'unauthorize-s'
-        self.UsersCommand = 'users-s'
-        self.ShellCommand = 'shell-s'
+        self.CloneCommand = 'clonex'
+        self.CountCommand = 'count'
+        self.PermissionCommand = 'perm'
+        self.DeleteCommand = 'del'
+        self.AuthorizeCommand = 'authorize'
+        self.UnauthorizeCommand = 'unauthorize'
+        self.UsersCommand = 'users'
+        self.ShellCommand = 'shell'
         self.PingCommand = 'ping'
-        self.LogCommand = 'log-s'
+        self.LogCommand = 'log'
         self.HelpCommand = 'help'
 
 BotCommands = _BotCommands()
